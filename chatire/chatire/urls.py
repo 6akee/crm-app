9f2645dfc5ec7e096f3f2ccec89a09fb332f6601
Bauyrzhan Almas <almassov.bauyrzhan@gmail.com>
2024-04-25 16:59:22 +0500

    Delete chatire directory

--- chatire/chatire/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-]
