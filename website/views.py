4ade31e5a506fb3d910fbc14d8cddbc0c3281fa7
6akee <almassov.bauyrzhan@gmail.com>
2024-04-30 16:08:03 +0500

    feat: new url customer_record and template'

--- website/views.py
+++ website/views.py
@@ -53,3 +53,13 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to View that page")
+        return redirect('home')
