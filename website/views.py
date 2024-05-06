19b5391f58f7406e708a280e33ce708f9d4a2b44
6akee <almassov.bauyrzhan@gmail.com>
2024-05-06 15:37:38 +0500

    url for delete record

--- website/views.py
+++ website/views.py
@@ -63,3 +63,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in to View that page")
         return redirect('home')
+    
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
