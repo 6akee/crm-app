39bc29d147ce5b72f500c3d6e8bb2b1c04ab0a82
6akee <almassov.bauyrzhan@gmail.com>
2024-05-13 17:31:11 +0500

    update record

--- website/views.py
+++ website/views.py
@@ -90,3 +90,17 @@ def add_record(request):
     else:
         messages.success(request, "You must be logged in to do that")
         return redirect('home')
+    
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
