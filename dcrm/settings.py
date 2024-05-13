7b0af2dd99323e1a4704dc0ab45386c566f75c81
6akee <almassov.bauyrzhan@gmail.com>
2024-05-13 17:54:28 +0500

    allowed host to list

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-&m#rr9o)c23q7!mw3&&%+ih4p+0pis=wu%p*ysi=xazcqdn-ix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = list[str]
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
