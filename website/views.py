af8f220378568c772168b0f5087ee3e0cac052aa
6akee <almassov.bauyrzhan@gmail.com>
2024-05-13 17:15:11 +0500

    feat: add record data form

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -71,6 +71,22 @@ def delete_record(request, pk):
         delete_it.delete()
         messages.success(request, "Record deleted successfully!")
         return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
+
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added!")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
     else:
         messages.success(request, "You must be logged in to do that")
         return redirect('home')
